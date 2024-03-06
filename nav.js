5cc8ca627aa30189fa996ab4165f2836d15f64a6
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-03-06 20:17:07 +0545

    navigation added

--- nav.js
+++ nav.js
@@ -1,17 +1,21 @@
 class nav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `<header>
-    <section class="header">
-    <div class="logo"></div>
-    <div class="navigation">
-      <nav>
-        <ul>
-          <li><a href="./index.html">Home</a></li>
-          <li><a href="./gallery.html">Gallery</a></li>
-          <li><a href="./about.html">About</a></li>
-          <li><a href="./contact.html">Contact</a></li>
-        </ul>
-      </nav>
+    <section class="header-section">
+    <div class="header">
+      <div class="logo">
+        <img src="./static/images/logo.jpeg" alt="" />  
+      </div>
+      <div class="navigation">
+        <nav>
+          <ul>
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./gallery.html">Gallery</a></li>
+            <li><a href="./about.html">About</a></li>
+            <li><a href="./contact.html">Contact </a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </section>
   </header>
